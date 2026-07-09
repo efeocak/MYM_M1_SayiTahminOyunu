275322dec71b7800130591183e50c8c4f6a6b541
Efe Ocak <efeocakis@gmail.com>
2026-07-09 12:16:24 +0300

    Ders2 - Ders İçi Çalışmalar

---
+++ src/ders2/D123_TipCikarimi.kt
@@ -0,0 +1,17 @@
+package ders2
+
+fun main() {
+    val sehir = "Ankara"
+    val sicaklik = 15
+    val nemOrani = 0.45
+    val dogruMu = true
+
+    var sistemMesaji: String
+    sistemMesaji = "Sıcaklık Uyarı Seviyesinde"
+
+    println("Mesaj: " + sistemMesaji)
+
+
+
+
+}
