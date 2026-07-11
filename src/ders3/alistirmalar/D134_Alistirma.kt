08abcef32cf82a397a2affd03fd9d8a28bbb1f96
Efe Ocak <efeocakis@gmail.com>
2026-07-11 23:52:32 +0300

    Ders3 - Alıştırmalar

---
+++ src/ders3/alistirmalar/D134_Alistirma.kt
@@ -0,0 +1,15 @@
+package ders3.alistirmalar
+
+fun main() {
+    print("Adınızı Giriniz: ")
+    val kullaniciAdi = readln()
+
+    print("Yaşınızı Giriniz: ")
+    val yas = readln().toDouble()
+
+    println("Merhaba $kullaniciAdi! Demek $yas yaşındasın.")
+
+
+
+
+}
